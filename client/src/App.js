dc3cea7b9cbb1714e0ce6be9d3a3bf41b2a38608
Agnieszka Matyja <agmatyja@gmail.com>
2019-07-11 12:40:04 +0200

    mern is working

---
+++ client/src/App.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+import MainLayout from './components/layout/MainLayout/MainLayout';
+
+// import routes
+import Home from './components/pages/Home/HomePage';
+import SinglePost from './components/pages/SinglePost/SinglePost';
+import AddPost from './components/pages/AddPost/AddPost';
+import Posts from './components/pages/Posts/PostsPage';
+import Contact from './components/pages/Contact/ContactPage';
+import NotFound from './components/pages/NotFound/NotFoundPage';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <MainLayout>
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/posts" exact component={Posts} />
+          <Route path="/contact" exact component={Contact} />
+		  <Route path="/posts/:id" exact component={SinglePost} />
+		  <Route path="/posts/new" exact component={AddPost} />
+          <Route component={NotFound} />
+        </Switch>
+      </MainLayout>
+    );
+  }
+
+}
+
+export default App;
